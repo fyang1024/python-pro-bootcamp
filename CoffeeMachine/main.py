5d58c111def780e43054a0cc805ebf94425ee8b1
fyang1024 <fyang1024@gmail.com>
2021-10-24 11:18:01 +1100

    refactoring

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -30,82 +30,87 @@ resources = {
     "coffee": 100,
 }
 
-currency_value = {
+coins = {
     "quarters": 0.25,
     "dimes": 0.10,
     "nickles": 0.05,
     "pennies": 0.01
 }
 
-money = 0
+revenue = 0
 
 
 def report():
     print(f"Water: {resources['water']}ml")
     print(f"Milk: {resources['milk']}ml")
     print(f"Coffee: {resources['coffee']}g")
-    print(f"Money: ${money}")
+    print(f"Money: ${revenue}")
 
 
-def check_resources(drink):
-    ingredients = MENU[drink]["ingredients"]
-    for key in resources:
-        if key in ingredients and resources[key] < ingredients[key]:
+def is_resources_sufficient(available_res, required_res):
+    for key in available_res:
+        if key in required_res and available_res[key] < required_res[key]:
             print(f"Sorry there is not enough {key}.")
             return False
     return True
 
 
-def take_money():
+def take_payment():
+    print("Please insert coins.")
     total = 0
-    for currency in currency_value:
-        units = get_number_input(currency)
-        total += float(units) * currency_value[currency]
+    for coin in coins:
+        units = get_number_input(coin)
+        total += float(units) * coins[coin]
     return total
 
 
-def get_number_input(units):
+def get_number_input(coin):
+    """Ask user to input non-negative whole number"""
     while True:
-        n = input(f"Please insert {units}:")
+        n = input(f"How many {coin}?:")
         try:
             if int(n) >= 0:
-                print(f"Received {n} {units}, or ${float(n) * currency_value[units]}")
+                print(f"Received {n} {coin}, or ${float(n) * coins[coin]}")
                 return n
         except ValueError:
             print("Please enter a non-negative whole number, e.g., 0, 1, 2,...")
 
 
-def check_money(drink, money_inserted):
-    if money_inserted < MENU[drink]["cost"]:
+def is_payment_sufficient(cost, payment):
+    """check if the payment is sufficient for the drink ordered."""
+    if payment < cost:
         print("Sorry that's not enough money. Money refunded.")
         return False
     return True
 
 
-def make_coffee(drink):
-    ingredients = MENU[drink]["ingredients"]
+def make_coffee(drink, ingredients):
     for key in resources:
         if key in ingredients:
             resources[key] -= ingredients[key]
     print(f"Here is your {drink}. Enjoy!")
 
 
-while True:
+is_on = True
+
+while is_on:
     choice = input("What would you like? (espresso/latte/cappuccino):")
     if choice == 'off':
         print("Bye!")
-        exit()
+        is_on = False
     elif choice == 'report':
         report()
     elif choice in MENU:
-        if check_resources(choice):
-            money_taken = take_money()
-            print(f"Received ${money_taken}")
-            if check_money(choice, money_taken):
-                money += MENU[choice]["cost"]
-                change = money_taken - MENU[choice]["cost"]
+        ingredients = MENU[choice]["ingredients"]
+        if is_resources_sufficient(resources, ingredients):
+            payment = take_payment()
+            print(f"Received ${payment}")
+            cost = MENU[choice]["cost"]
+            if is_payment_sufficient(cost, payment):
+                revenue += cost
+                change = round(payment - cost, 2)
                 if change > 0.01:
                     print(f"Here is ${change} dollars in change.")
-                make_coffee(choice)
+                make_coffee(choice, ingredients)
     else:
         print("Sorry I don't understand your instruction")
